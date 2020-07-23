f1ba4694d742c8a5b4a55ddf3f6d39c0c69d1b36
kirill <Kirill>
2020-07-23 15:17:23 +0600

    update

--- web/nodeapp/bookcases.py
+++ web/nodeapp/bookcases.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from .dynamic_bookcases import make_dynamic_bookcase
 
 CAT_NUM = 10
 
@@ -37,6 +38,40 @@ def load_bookcases(raw_query, companies):
         cc.append(cid)
         bookcase_classic_not_filtered[bc] = cc
 
+    elastic_ids = set()
+    for cid in companies:
+        elastic_ids.add(cid)
+    dynamic_bookcases = make_dynamic_bookcase(companies=None, elastic_ids=elastic_ids)
+
+    for db in dynamic_bookcases:
+        if db['percentage']==0.15:
+            print('YES')
+            print(db)
+
+            db_bookcases = db['bookcases']
+            for bid in db_bookcases:
+                shelf = db_bookcases[bid]
+                print(shelf)
+                for cid in shelf:
+                    if cid in companies:
+                        bc_companies[cid]['bookcase_query'] = excel_data_df.iloc[i, 0]
+
+                        cc = bookcase_query.get(bid, [])
+                        cc.append(cid)
+                        bookcase_query[bid] = cc
+
+                    cc = bookcase_query_not_filtered.get(bid, [])
+                    cc.append(cid)
+                    bookcase_query_not_filtered[bid] = cc
+            break
+    print('QQ')
+    print(elastic_ids)
+    print(bookcase_query_not_filtered1)
+    #print(from .dynamic_bookcases import make_dynamic_bookcase)
+
+
+
+    '''
     fname_query = os.path.join('support', 'node', 'node_' + str(raw_query), '0.15',
                                'shelve for each company extended.csv')
     excel_data_df = pd.read_csv(fname_query, delimiter=';')
@@ -56,5 +91,5 @@ def load_bookcases(raw_query, companies):
         cc = bookcase_query_not_filtered.get(bc, [])
         cc.append(cid)
         bookcase_query_not_filtered[bc] = cc
-
+    '''
     return bc_companies, bookcase_classic, bookcase_query, bookcase_classic_not_filtered, bookcase_query_not_filtered
