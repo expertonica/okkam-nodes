e298d2e97e943fa4a0f7f1090f142697e59d5eb6
kirill <Kirill>
2020-08-16 21:02:36 +0600

    added title in request

--- web/nodeapp/mappers.py
+++ web/nodeapp/mappers.py
@@ -1,6 +1,26 @@
+import re
+from pymystem3 import Mystem
+
 from .entities import CompanyTop6Entity, PageTop6Entity
 
+
+def query_in_title(m, title, raw_query):
+
+    query = set(m.lemmatize(raw_query))
+
+    n = 0
+    # TODO убрать лишние слова и прочее из загловка
+    title = re.sub('\W+', ' ', title).strip().lower()
+    lemmas = set(m.lemmatize(title))
+
+    for q in query:
+        if q in lemmas:
+            n += 1
+
+    return n
+
 def map_get_nodes(json_data):
+    m = Mystem()
     companies = {}
     companies_json_data = json_data['companies']
     query = json_data['query']
@@ -8,6 +28,7 @@ def map_get_nodes(json_data):
         company_id = c_data['company_id']
         top6 = []
         for p_data in c_data['top_6']:
-            top6.append(PageTop6Entity(p_data['url'], p_data['es_score'], p_data['query_in_title']))
+            qit = query_in_title(m, p_data['title'], query)
+            top6.append(PageTop6Entity(p_data['url'], p_data['es_score'], qit))
         companies[company_id] = CompanyTop6Entity(company_id, top6)
     return query, companies
