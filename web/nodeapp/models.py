2928686ad0ac948de4dcb148c08fe8754e482b73
Kirill <kirill@expertonica.com>
2020-09-24 18:51:52 +0600

    postgress added

--- web/nodeapp/models.py
+++ web/nodeapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models import JSONField
 
 # Create your models here.
+
+class UrlWc(models.Model):
+    url = models.CharField(max_length=2048, db_index=True)
+    company_id = models.IntegerField(db_index=True)
+    company_url = models.CharField(max_length=128)
+    word_counter = JSONField()
+
